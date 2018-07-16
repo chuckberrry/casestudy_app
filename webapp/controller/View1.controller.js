5696000cbe39feb3713c882f6613095e51230dfb
S0019269689 <armin.hatting@cbs-consulting.de>
2018-07-16 08:39:17 +0000

    Merge branch 'master'

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -5,5 +5,14 @@ sap.ui.define([
 
 	return Controller.extend("cs.case_study.controller.View1", {
 
+		onInit: function () {
+/*			var oModel = new sap.ui.model.json.JSONModel();
+			oModel.loadData('/AWS_case_study/res.json');
+			// Assign the model object to the SAPUI5 core
+			sap.ui.getCore().setModel(oModel);*/
+
+			// create the FileUploader control
+		}
+
 	});
 });
