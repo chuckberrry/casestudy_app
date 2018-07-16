00a3650e00d0aa53d6b943c507456f62978c2086
S0019269689 <armin.hatting@cbs-consulting.de>
2018-07-16 09:09:35 +0000

    initial commit

---
+++ webapp/controller/View1.controller.js
@@ -0,0 +1,18 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("cs.case_study.controller.View1", {
+
+		onInit: function () {
+/*			var oModel = new sap.ui.model.json.JSONModel();
+			oModel.loadData('/AWS_case_study/res.json');
+			// Assign the model object to the SAPUI5 core
+			sap.ui.getCore().setModel(oModel);*/
+
+			// create the FileUploader control
+		}
+
+	});
+});
